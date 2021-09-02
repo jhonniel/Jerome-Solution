42b711e33384fc81ad81fa89d6b07e73f1c3e994
unknown <jhonnielygay99@gmail.com>
2021-09-02 21:00:32 +0800

    jerome

---
+++ Jerome Solution_Ygay/frmMain.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtItemPrice = New System.Windows.Forms.TextBox()
+        Me.lblSub = New System.Windows.Forms.TextBox()
+        Me.lblSaleTax = New System.Windows.Forms.TextBox()
+        Me.lblShip = New System.Windows.Forms.TextBox()
+        Me.lblTotalDue = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(91, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Item price:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Subtotal:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Sales tax:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 76)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Shipping:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(8, 102)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(55, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Total due:"
+        '
+        'txtItemPrice
+        '
+        Me.txtItemPrice.Location = New System.Drawing.Point(94, 45)
+        Me.txtItemPrice.Name = "txtItemPrice"
+        Me.txtItemPrice.Size = New System.Drawing.Size(61, 20)
+        Me.txtItemPrice.TabIndex = 5
+        '
+        'lblSub
+        '
+        Me.lblSub.Location = New System.Drawing.Point(68, 21)
+        Me.lblSub.Name = "lblSub"
+        Me.lblSub.ReadOnly = True
+        Me.lblSub.Size = New System.Drawing.Size(71, 20)
+        Me.lblSub.TabIndex = 6
+        '
+        'lblSaleTax
+        '
+        Me.lblSaleTax.Location = New System.Drawing.Point(68, 47)
+        Me.lblSaleTax.Name = "lblSaleTax"
+        Me.lblSaleTax.ReadOnly = True
+        Me.lblSaleTax.Size = New System.Drawing.Size(71, 20)
+        Me.lblSaleTax.TabIndex = 7
+        '
+        'lblShip
+        '
+        Me.lblShip.Location = New System.Drawing.Point(68, 73)
+        Me.lblShip.Name = "lblShip"
+        Me.lblShip.ReadOnly = True
+        Me.lblShip.Size = New System.Drawing.Size(71, 20)
+        Me.lblShip.TabIndex = 8
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.BackColor = System.Drawing.SystemColors.Window
+        Me.lblTotalDue.Location = New System.Drawing.Point(68, 99)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(71, 20)
+        Me.lblTotalDue.TabIndex = 9
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(42, 247)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(65, 25)
+        Me.btnCalc.TabIndex = 10
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(140, 247)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(65, 25)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblTotalDue)
+        Me.GroupBox1.Controls.Add(Me.lblShip)
+        Me.GroupBox1.Controls.Add(Me.lblSaleTax)
+        Me.GroupBox1.Controls.Add(Me.lblSub)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(42, 83)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(163, 139)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(250, 303)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.txtItemPrice)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmMain"
+        Me.Text = "Jerome's"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtItemPrice As TextBox
+    Friend WithEvents lblSub As TextBox
+    Friend WithEvents lblSaleTax As TextBox
+    Friend WithEvents lblShip As TextBox
+    Friend WithEvents lblTotalDue As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Jerome Solution_Ygay/frmMain.vb
@@ -0,0 +1,76 @@
+﻿
+' Name:         Jerome
+' Purpose:      Displays gross pay
+' Programmer:   <Jhonniel Ygay> on <Sep 6,2020>
+
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+
+
+Public Class frmMain
+
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+    Private Sub txtItemPrice_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtItemPrice.KeyPress
+
+        If (Asc(e.KeyChar) >= 48 And Asc(e.KeyChar) <= 57) Or Asc(e.KeyChar) = 46 Or Asc(e.KeyChar) = 8 Then
+
+        Else
+            e.Handled = True
+        End If
+
+    End Sub
+
+    Private Sub txtItemPrice_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtItemPrice.Enter
+        txtItemPrice.SelectAll()
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private dblItemPrice, dblSubPrice As Double
+    Private dblSalesTax, dblShipping, dblTotalDue As Double
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        System.Threading.Thread.CurrentThread.CurrentCulture = New System.Globalization.CultureInfo("en-US")
+
+
+
+        Double.TryParse(txtItemPrice.Text, dblItemPrice)
+
+        dblSubPrice += dblItemPrice
+
+        dblSalesTax = dblSubPrice * 0.05
+
+
+
+        If dblSubPrice >= 100 Then
+
+            dblShipping = 0
+
+        Else
+
+            dblShipping = 6.5
+
+        End If
+        dblTotalDue = dblSubPrice + dblSalesTax + dblShipping
+
+        lblSub.Text = Format(dblSubPrice, "C2")
+        lblSaleTax.Text = Format(dblSalesTax, "C2")
+        lblShip.Text = Format(dblShipping, "C2")
+        lblTotalDue.Text = Format(dblTotalDue, "C2")
+
+
+        txtItemPrice.Focus()
+
+    End Sub
+
+
+    Private Sub txtItemPrice_TextChanged(sender As Object, e As EventArgs) Handles txtItemPrice.TextChanged
+
+    End Sub
+End Class
